ced575a09cfc141d23fd98aa1ef7016d5b32a42d
JuliaIzotova] <yuizotova15@gmail.com>
2022-04-27 13:17:23 +0300

    afisha

---
+++ src/main/java/ru/netology/domain/Movies.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class Movies {
+    private int id;
+    private String movieName;
+
+    public Movies(int id, String movieName) {
+        this.id = id;
+        this.movieName = movieName;
+    }
+}
+
